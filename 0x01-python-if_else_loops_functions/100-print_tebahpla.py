401fe2bf035b4a4951cb4a72394f1e9d5008152a
Agure-la <lameckagure@gmail.com>
2023-08-08 06:49:49 -0700

    Fixed Identation Error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Author - Agure Lameck
+# Author -Agure Lameck
 
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Author - Agure Lameck
+# Author - Agure Lameck
 
 def magic_calculation(a, b, c):
     """Match bytecode provided by Holberton School."""
